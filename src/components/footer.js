f869bd749858ad21358c2f8560ce28ee547911b6
Mehar Fatma <mehar.fatma202@gmail.com>
2025-02-11 00:38:44 +0530

    minor change

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <div className="text-[#1f2937] mt-1 absolute bottom-1 w-full text-center">
+        <div className="text-[#1f2937] absolute bottom-2 w-full text-center">
             <div>
                 Made with ❤️ by{" "}
                 <a rel="noreferrer" href="https://github.com/itsmeharfatma" target="_blank" className="from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent hover:underline">
